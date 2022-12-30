13c6f0f37a1ddcb75da4e32dd052f6ad79946d58
e-kornmann <elton.kornmann@gmail.com>
2022-12-31 00:27:39 +0100

    Initial commit

---
+++ functions.js
@@ -0,0 +1,10 @@
+// const getCompanyName = () => "SALT";
+// console.log("salt> Functions loaded")
+
+myFunctions = {
+    getCompanyName : function ()  {
+      return "SALT";
+    }
+  }
+  console.log("salt> Functions loaded");
+    
