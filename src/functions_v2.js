0f3c8f57f30a7e15f63ea0b31ef9e3f1a5be6f32
e-kornmann <elton.kornmann@gmail.com>
2023-01-04 01:32:40 +0100

    Add a-z filter

--- src/functions_v2.js
+++ src/functions_v2.js
@@ -1,43 +1,33 @@
 myFunctions = {
-  
-
- updateList : function (anyList) {
+  updateList : function (anyList) {
     const userList = myFunctions.createUserList(anyList);
     elSelector.innerHTML = ''; // Clear the current list
     elSelector.appendChild(userList); // Append the new list
+    },
+
+ clearInput : function () {
+    var getValue= document.getElementById("searchbar");
+        if (getValue.value !="") {
+            getValue.value = "";
+        }
+ },
+
+  searchFilter : function (lijst) {
+    let filteredList = lijst;
+
+    const search = () => {
+      const input = document.getElementById('searchbar').value.toLowerCase();
+      filteredList = lijst.filter(user => user.name.first.toLowerCase().includes(input));
+    }
+    search();
+    return filteredList;
   },
-
-
-
-
-  
-  
-
-searchFilter : function (lijst) {
-  let filteredList = lijst;
-
-  const search = () => {
-    const input = document.getElementById('searchbar').value.toLowerCase();
-    filteredList = lijst.filter(user => user.name.first.toLowerCase().includes(input));
-  }
-  search();
-      
-  return filteredList;
-  },
-  
-
-
-
 
   filters : function (lijst) {
-   
-    // declare gender
+    // checkbox booleans
     const ageCheckbox = (document.querySelector("input[name=age]").checked);
-    // both checkboxes
     const maleCheckbox = (document.querySelector("input[name=male]").checked);
     const femaleCheckbox = (document.querySelector("input[name=female]").checked);
-    // turn the other one false and the other way around
-    
     // active filters
     let filteredList = lijst;
     let maleFilter = lijst.filter(user => user.gender === 'male');
@@ -47,132 +37,108 @@ searchFilter : function (lijst) {
     let fAgeFilter = ageFilter.filter(user => user.gender === 'female'); 
     let mAgeFilter = ageFilter.filter(user => user.gender === 'male'); 
 
-
-
-
+    // checkbox combinations
+    if (!maleCheckbox && !femaleCheckbox && !ageCheckbox) { filteredList = lijst; } 
+    if (maleCheckbox && femaleCheckbox && ageCheckbox)    { filteredList = ageFilter; }
+    if (maleCheckbox && !femaleCheckbox && !ageCheckbox)  { filteredList = maleFilter; }
     
+    if (!maleCheckbox && femaleCheckbox && !ageCheckbox)  { filteredList = femaleFilter; }
+    if (!maleCheckbox && !femaleCheckbox && ageCheckbox)  { filteredList = ageFilter; }
 
-
-    //filters
-    //triple
-    if (!maleCheckbox && !femaleCheckbox && !ageCheckbox) {
-      filteredList = lijst;
-    } 
-    if (maleCheckbox && femaleCheckbox && ageCheckbox) {
-      filteredList = ageFilter;
-    }
-    
-    //singles
-    if (maleCheckbox && !femaleCheckbox && !ageCheckbox) {
-      filteredList = maleFilter;
-    }
-    if (!maleCheckbox && femaleCheckbox && !ageCheckbox) {
-      filteredList = femaleFilter;
-    }
-    if (!maleCheckbox && !femaleCheckbox && ageCheckbox) {
-      filteredList = ageFilter;
-    }
-
-    // duos
-    if (!maleCheckbox && femaleCheckbox && ageCheckbox) {
-      filteredList = fAgeFilter;
-    }
-    if (maleCheckbox && !femaleCheckbox && ageCheckbox) {
-      filteredList = mAgeFilter;
-    }
-    if (maleCheckbox && femaleCheckbox && !ageCheckbox) {
-      filteredList = lijst;
-    }
+    if (!maleCheckbox && femaleCheckbox && ageCheckbox)   { filteredList = fAgeFilter; }
+    if (maleCheckbox && !femaleCheckbox && ageCheckbox)   { filteredList = mAgeFilter; }
+    if (maleCheckbox && femaleCheckbox && !ageCheckbox)   { filteredList = lijst; }
        
     return filteredList;
-    },
+  },
   
-
-  
-
   filterTrigger : function (lijst) {
 
-    // apply filters
-    let filtered = myFunctions.filters(lijst);
-       // age filter
+    // age filter
     const checkbox = document.querySelector("input[name=age]");
-    checkbox.addEventListener('click', function() {
+    checkbox.addEventListener("click", function() {
+    myFunctions.clearInput();
     myFunctions.updateList(myFunctions.filters(lijst));
     });
 
     // male filter
     const checkboxm = document.querySelector("input[name=male]");
-    checkboxm.addEventListener('click', function() {
+    checkboxm.addEventListener("click", function() {
+    myFunctions.clearInput();
     myFunctions.updateList(myFunctions.filters(lijst));
     });
   
     // female filter
     const checkboxf = document.querySelector("input[name=female]");
-    checkboxf.addEventListener('click', function() {
+    checkboxf.addEventListener("click", function() {
+    myFunctions.clearInput();
     myFunctions.updateList(myFunctions.filters(lijst));
     });
 
     // search filter
-    const searchbox = document.getElementById('searchbar');
-    searchbox.addEventListener('keyup', () => {
+    const input = document.getElementById("searchbar");
+    input.addEventListener("keypress", function(event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("btnSearch").click();
+      }
+    });
+    const searchbox = document.getElementById("btnSearch");
+    searchbox.addEventListener("click", function() {
       myFunctions.updateList(myFunctions.searchFilter(myFunctions.filters(lijst)));
     });
-    
+
+    // sort filter
+    // add the a-z filter
+    const sortAlpha = document.getElementById("az");
+    sortAlpha.addEventListener("click", function() {
+      myFunctions.clearInput();
+      lijst.sort((a, b) => a.name.first.localeCompare(b.name.first))
+      myFunctions.updateList(myFunctions.filters(lijst))
+    });
   },
 
-  
-   
-  
   // create header
-    createUserNameHeader : function (user) {
-      const userHeading = document.createElement('h2');
-      userHeading.textContent = `${user.name.first} ${user.name.last}`
-      return userHeading;
-    },
+  createUserNameHeader : function (user) {
+    const userHeading = document.createElement('h2');
+    userHeading.textContent = `${user.name.first} ${user.name.last}`
+    return userHeading;
+  },
+
+  // create image
+  createUserImage : function (user) {
+    const userImage = document.createElement('img');
+    userImage.src = `${user.picture.large}`;
+    return userImage;
+  },
+
+  // create persona
+  createPersona : function (user) {
+    const userDiv = document.createElement('div');
+    userDiv.classList.add('persona','cta','bg', 'male');
+     if (user.gender == 'female') {
+     userDiv.classList.add('female');
+     }
+    return userDiv;
+  },
   
-    // create image
-    createUserImage : function (user) {
-      const userImage = document.createElement('img');
-      userImage.src = `${user.picture.large}`;
-      return userImage;
-    },
-  
-    // create persona
-    createPersona : function (user) {
-      const userDiv = document.createElement('div');
-      userDiv.classList.add('persona','cta','bg', 'male');
-      if (user.gender == 'female') {
-      userDiv.classList.add('female');
-      }
-      return userDiv;
-    },
-  
-  
-    // ---------- data.results is a callback passed to users
-    createUserList: function (users)  {
-  
-      const userList = document.createElement('div');
-      userList.classList.add('wrapper');
+  // ---------- data.results is a callback passed to users
+  createUserList: function (users)  {
+    const userList = document.createElement('div');
+    userList.classList.add('wrapper');
   
     for (let i = 0; i < users.length; i++) {
-     
       const userDiv = myFunctions.createPersona(users[i]); 
-  
       userDiv.appendChild(myFunctions.createUserNameHeader(users[i]));                       
       userDiv.appendChild(myFunctions.createUserImage(users[i]));  
-      
       userList.appendChild(userDiv);
     }
-  
     return userList;
   },
   
-  
-  
-  
-    //  FETCH
-    getUsers: function (element) {
-    fetch('https://randomuser.me/api/?results=52')
+  //  FETCH
+  getUsers: function (element) {
+    fetch('https://randomuser.me/api/?results=502')
       .then(response => response.json())
       
       .then(data => { 
@@ -183,13 +149,14 @@ searchFilter : function (lijst) {
            
         // Add the event listener after the list is created
         myFunctions.filterTrigger(data.results);
-        
+               
       });
       
   }
   
   //end of myFunctions:
-   }
-      console.log("salt> Functions loaded");
+}
+
+console.log("salt> Functions loaded");
       
   
